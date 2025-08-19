ae1f35731204bac0d4b9afd7e529ffb9cf853180
varshitha-besthu <varshithabesthavemula@gmail.com>
2025-08-19 18:27:32 +0530

    updated uploadworker

--- frontend/src/utils/uploadworker.ts
+++ frontend/src/utils/uploadworker.ts
@@ -4,7 +4,7 @@ const DB_NAME = "recordingDB";
 const STORE_NAME = "chunks";
 const DB_VERSION = 1;
 let isUploading = 0;
-
+const BackendUrl = import.meta.env.VITE_BACKEND_URL;
 function openDB(): Promise<IDBDatabase> {
   return new Promise((resolve, reject) => {
     const req = indexedDB.open(DB_NAME, DB_VERSION);
@@ -122,7 +122,7 @@ async function uploadChunkToBackend(chunkData: any) {
 
     console.log("sending the data to the backend");
 
-    const res = await axios.post("http://localhost:3000/api/upload_chunk", formData  , {
+    const res = await axios.post("${}/api/upload_chunk", formData  , {
         headers: { "Content-Type": "multipart/form-data" },
     });
 
