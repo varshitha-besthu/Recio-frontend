693474baf2b3e44cf8b0688b1176e10cce0bc058
varshitha-besthu <varshithabesthavemula@gmail.com>
2025-09-13 14:13:34 +0530

    Fetched the participantName from search Params in callback.tsx

--- frontend/src/pages/CallBack.tsx
+++ frontend/src/pages/CallBack.tsx
@@ -7,18 +7,19 @@ export default function Callback() {
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const token = params.get("token");
+    const participantName = params.get("participantName") || "";
 
     if (token) {
       localStorage.setItem("token", token);
+      localStorage.setItem("participantName", participantName);
       const savedRoute = localStorage.getItem("isFromProtectedRoute");
       if (savedRoute) {
-
         localStorage.removeItem("isFromProtectedRoute");
         navigate(savedRoute);
       } else {
         navigate("/preStudio");
       }
-    }
+    } 
   }, [navigate]);
 
   return <div>Signing you in with Google...</div>;
